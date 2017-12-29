a22bb82ddd123567c41527691592bb0f988d1106
Kenneth Lehr <kenneth.c.lehr@gmail.com>
2017-12-29 23:16:41 +0000

    automatically backed up by learn

--- konami_code.js
+++ konami_code.js
@@ -2,7 +2,8 @@ const code = [38, 38, 40, 40, 37, 39, 37, 39, 66, 65]
 
 function konami(e) {
     const key = parseInt(e.detail || e.which)
-    let index = 0 
+    let index = 0; 
+    
     if (key == code[index]) {
       index++ 
       
