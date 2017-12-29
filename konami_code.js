8d13a7217c9dc1e62883b7b0abc1e834e6c84410
Kenneth Lehr <kenneth.c.lehr@gmail.com>
2017-12-29 23:08:23 +0000

    automatically backed up by learn

--- konami_code.js
+++ konami_code.js
@@ -3,7 +3,9 @@ const code = [38, 38, 40, 40, 37, 39, 37, 39, 66, 65]
 function init() {
   const body = querySelector('body')
   
-  
+  function konami(e) {
+    
+  }
   
   body.addEventListner('keydown', alert)
 
